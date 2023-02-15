ecb94a74506daf6d50ddac7f88a06021ee526d6a
suganya-pg <suganya_cs@rvsgroup.com>
2023-02-15 14:25:29 +0530

    test.js

--- test.js
+++ test.js
@@ -10,7 +10,7 @@ describe("Example Test 1", () => {
     const mockResponse = httpMocks.createResponse();
     exampleRouteHandler(mockRequest, mockResponse);
     const actualResponseBody = mockResponse._getData();
-    const expectedResponseBody = "Goodbye Earthling!!!!!!!";
+    const expectedResponseBody = "Goodbye Earthling!!!!";
     assert(actualResponseBody, expectedResponseBody);
   });
 });
